08fb9fc314163f0a7c29167092109d8a4653bce1
redsnowc <nuanyang.44@gmail.com>
2019-06-07 23:04:07 +0800

    Create wsgi.py

---
+++ wsgi.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+    :author: 秋荏苒
+    :copyright: © 2019 by 秋荏苒 <nuanyang.44@gmail.com>.
+    :license: MIT, see LICENSE for more details.
+"""
+import os
+
+from dotenv import load_dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
+from app import create_app
+
+app = create_app()
